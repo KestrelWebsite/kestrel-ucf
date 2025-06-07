78e6961b8952c5340062baa3f33f9491855845aa
kamillamamatova <kamillamamatova.km@gmail.com>
2025-06-07 18:38:46 -0400

    Added "Contributors Card" layout for all contributor card

---
+++ app/contributions/_components/ContributorsCard.tsx
@@ -0,0 +1,45 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import{
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+export interface ContributorsCardProps{
+    title: string;
+    description: string;
+    image: StaticImageData;
+    link: string;
+}
+
+const ContributorsCard = ({ title, description, image, link}: ContributorsCardProps) => {
+    return(
+        <Card className = "w-full max-w-xs bg-slate-600 border-slate-700 hover:scale-102 hover:shadow-xl transition-transform">
+            <CardHeader>
+                <Image
+                    className = "rounded-lg mb-4 shadow-md transition-all duration-300"
+                    src = {image}
+                    alt = {title}
+                />
+                <CardTitle className = "text-2xl text-white">
+                    {title}
+                </CardTitle>
+                <CardDescription className = "overflow-y-auto max-h-[300px] text-white/50">
+                    {description}
+                </CardDescription>
+            </CardHeader>
+            <CardContent />
+            <CardFooter>
+                <Link href = {link} className = "text-blue-300 hover:underline text-sm">
+                    Learn More
+                </Link>
+            </CardFooter>
+        </Card>
+    );
+};
+
+export default ContributorsCard;
