56fbfa54c4cd7ed0cfda01401dad5b846a0a9e52
boop123-dotcom <abigailannroyce@gmail.com>
2025-11-03 15:03:45 -0500

    added minimal default export to devlog page

--- app/admin/devlogs/[id]/page.tsx
+++ app/admin/devlogs/[id]/page.tsx
@@ -1,32 +1,3 @@
-/*
-
-import { getDevlogById } from "@/app/api/devlogs/hooks";
-import { notFound } from "next/navigation";
-import AdminDevlogDetails from "./AdminDevlogDetails";
-
-interface Props {
-  params: Promise<{ id: string }>;
+export default function DevlogPlaceholderPage() {
+  return null;
 }
-
-const DevlogEditPage = async (props: Props) => {
-  const params = await props.params;
-  const devlogId = params.id;
-  if (!devlogId) {
-    return notFound();
-  }
-
-  const devlog = await getDevlogById(devlogId);
-  if (!devlog) {
-    return notFound();
-  }
-
-  return <AdminDevlogDetails devlog={devlog} />;
-};
-
-export const dynamic = "force-dynamic";
-
-export default DevlogEditPage;
-*/
-
-// keep file valid
-//export const placeholder = true;
